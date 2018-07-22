e675a26c1c409085658622885343a5082e8605c2
Mauri de Souza Nunes <mauri870@gmail.com>
2018-07-21 23:25:52 -0300

    Allow testing of a single program

--- test_runner.sh
+++ test_runner.sh
@@ -4,16 +4,24 @@ set -e
 TESTDIR=tests
 SRCDIR=src
 
+function build_and_test() {
+    echo "Compiling \"$1\"..."
+    PROGRAM="$1" make
+
+    echo "Running test for \"$1\"..." 
+    bats "$TESTDIR/$1.bats"
+}
+
 make clean > /dev/null
 
-for DIR in $(ls -d $SRCDIR/*/)
+for PROGRAM in $(ls -d src/*/ | cut -f2 -d'/')
 do
-    PROGRAM=$(basename "$DIR")
-    echo "Compiling $PROGRAM..."
-    PROGRAM="$PROGRAM" make
+    if [ $# -eq 1 ] && [ "$1" != "$PROGRAM" ]
+    then
+        continue
+    fi
 
-    echo "Running test for $PROGRAM..." 
-    bats "$TESTDIR/$PROGRAM.bats"
+    build_and_test "$PROGRAM"
 done
 
 make clean > /dev/null
